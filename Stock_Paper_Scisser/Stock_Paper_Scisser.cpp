259009b1c29d78673efc11df7adf5b86189e3506
MohamedMostafa-1 <mohamedmostafajalal@gmail.com>
2026-04-10 21:04:06 +0200

    start project Stock_Paper_Scisser

---
+++ Stock_Paper_Scisser/Stock_Paper_Scisser.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int ReadNumberRound() {
+    int Number =0;
+    cout << "Please Enter How Many Round You Want To Play? ";
+    cin >> Number;
+    return Number;
+}
+
+
+int main()
+{
+    cout << "Hello World!\n";
+}
